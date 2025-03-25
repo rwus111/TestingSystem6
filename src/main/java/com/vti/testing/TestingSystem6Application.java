2aaeb228f69c46c9c6c0eab7db369d5075d1dbd2
cuongloveyou <qcuongdola1998@gmail.com>
2025-03-25 21:23:12 +0700

    first commit

---
+++ src/main/java/com/vti/testing/TestingSystem6Application.java
@@ -0,0 +1,13 @@
+package com.vti.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSystem6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSystem6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/vti/testing/TestingSystem6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystem6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
