d41a2bd8959f792b4b2c39262fee29032934aeac
cuongloveyou <qcuongdola1998@gmail.com>
2025-04-05 19:38:41 +0700

    add security account entity

--- src/main/java/com/vti/testing/entity/Account.java
+++ src/main/java/com/vti/testing/entity/Account.java
@@ -9,9 +9,25 @@ public class Account {
     private int id;
     @Column(length = 50, unique = true, nullable = false)
     private String username;
+    @Column(name = "`password`", length = 800, nullable = false)
+    private String password;
+    @Column(name = "`first_name`", length = 50, nullable = false)
+    private String firstName;
+    @Column(name = "`last_name`", length = 50, nullable = false)
+    private String lastName;
+    @Column(name = "`role`", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
     @ManyToOne
     @JoinColumn(name = "department_id", nullable = false)
     private Department department;
+
+    @PrePersist
+    public void prePersist() {
+        if (role == null) {
+            role = Role.EMPLOYEE;
+        }
+    }
 
     public int getId() {
         return id;
@@ -35,5 +51,37 @@ public class Account {
 
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
